d7eaa61240c9501cf4d5e7cc76656b1527712152
Jessica Bailey <baileyjessica2@gmail.com>
2017-08-31 19:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays(string) {
   return ("Happy holidays!")
 }
-function happyHolidaysTo(string) {
+function happyHolidaysTo(name) {
 var name = Jessica
-return ('Happy holidays, ${name}!')
+return ('Happy holidays, ${name}!')  
 }

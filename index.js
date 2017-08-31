ee69af3945fb1d444dd75b56752e89e8ff976cc5
Jessica Bailey <baileyjessica2@gmail.com>
2017-08-31 19:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ return `Happy holidays, ${name}!`
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(number, holiday) {
-  return `It's ${number} days until ${holiday}!`
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
